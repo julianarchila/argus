aa0f44e2903d7b5b321e1091bbdac132382bd7ba
Julian Archila <julianarchilacaro@gmail.com>
2025-05-07 22:21:18 -0500

    clean up

--- packages/core/src/feed-parser/feed-parser.ts
+++
@@ -1,7 +0,0 @@
-import { FeedItem, FeedParser } from "./types";
-
-export async function parseFeed(parser: FeedParser, lastProcessed?: Date | null): Promise<FeedItem[]> {
-  const items = await parser();
-  if (!lastProcessed) return items;
-  return items.filter(item => item.publicationDate && item.publicationDate > lastProcessed);
-}
