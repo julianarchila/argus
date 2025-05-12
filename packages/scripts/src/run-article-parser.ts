46480b122afdba218adc9b13864df104497c1a29
Julian Archila <julianarchilacaro@gmail.com>
2025-05-12 18:45:45 -0500

    feat(scripts): add article-parser script

---
+++ packages/scripts/src/run-article-parser.ts
@@ -0,0 +1,31 @@
+import { Resource } from "sst"
+
+import { getSiteConfig } from "@argus/core/parsers/index"
+
+
+// Get command line arguments (excluding 'node' and script name)
+const args = process.argv.slice(2);
+
+// Check if a value was provided
+if (args.length < 2) {
+  console.error("Error: you must provide a site name and a url")
+  process.exit(1);
+}
+
+// Get the first argument
+const siteName = args[0] || "";
+const url = args[1] || ""
+
+const siteConfig = getSiteConfig(siteName)
+
+if (!siteConfig) {
+  console.error("Error: no configuration registered for " + siteName)
+  process.exit(1);
+}
+
+const res = await siteConfig.articleParser.parse(url)
+
+console.log({
+  article: res
+})
+
